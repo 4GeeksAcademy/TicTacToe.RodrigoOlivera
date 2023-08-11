1755df08e95e67d8174d56df48a2d0424a235ad8
rocady21 <68871996+rocady21@users.noreply.github.com>
2023-08-11 18:37:00 +0000

    commit final del juego

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import LobbyGame from "./LobbyGame";
+import GameComponent from "./GameComponent";
 
 //create your first component
 const Home = () => {
+	// Defino un estado inicial que es el lobby y cambiara a "game" solo cuando ingrese ambos nombres y le de click al boton para iniciar
+	const [stateGame, setStateGame] = useState("Lobby")
+	const [Players, setPlayers] = useState("")
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container ">
+			<div className="row tittle">
+				<div className="col-12">
+					<h1 className="text-center mt-5 text-white">Tic Tac Toe in React.js</h1>
+				</div>
+			</div>
+			{
+				stateGame === "Lobby"? <LobbyGame goGame= {setStateGame} setPlayers={setPlayers}/> : <GameComponent Players={Players}/>
+			}
+
 		</div>
 	);
 };
